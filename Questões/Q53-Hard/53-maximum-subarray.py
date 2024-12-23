105e479bc2f58d282b7bbd26673fad17bd564b2f
Pedro Augusto Dourado Izarias <pedroaugustoizarias@gmail.com>
2024-12-23 11:27:04 -0300

    Update 53-maximum-subarray.py

--- Questões/Q53-Hard/53-maximum-subarray.py
+++ Questões/Q53-Hard/53-maximum-subarray.py
@@ -1,14 +1,16 @@
-def find_maximum_subarray(nums, start, end):
-    if start == end:  
-        return nums[start]
-
-    mid = (start + end) // 2
-    left_max = find_maximum_subarray(nums, start, mid)  
-    right_max = find_maximum_subarray(nums, mid + 1, end)  
-    cross_max = max_crossing_sum(start, mid, end, nums)  
-
-    return max(left_max, right_max, cross_max)
-
 def max_crossing_sum(start, mid, end, nums):
-    
-    pass
+   
+    left_sum = float('-inf')
+    total = 0
+    for i in range(mid, start - 1, -1):
+        total += nums[i]
+        left_sum = max(left_sum, total)
+
+  
+    right_sum = float('-inf')
+    total = 0
+    for i in range(mid + 1, end + 1):
+        total += nums[i]
+        right_sum = max(right_sum, total)
+
+    return left_sum + right_sum
