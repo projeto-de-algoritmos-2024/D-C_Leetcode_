05aac8fa33e5c67ec1497963fde7a6c4ee629d39
Pedro Augusto Dourado Izarias <pedroaugustoizarias@gmail.com>
2024-12-23 12:10:41 -0300

    Update 53-maximum-subarray.py

--- Questões/Q53-Hard/53-maximum-subarray.py
+++ Questões/Q53-Hard/53-maximum-subarray.py
@@ -1,5 +1,6 @@
 class Solution:
     def maxSubArray(self, nums):
+        
         return self.find_maximum_subarray(nums, 0, len(nums) - 1)
 
     def find_maximum_subarray(self, nums, start, end):
@@ -9,19 +10,19 @@ class Solution:
         mid = (start + end) // 2
         left_max = self.find_maximum_subarray(nums, start, mid)  
         right_max = self.find_maximum_subarray(nums, mid + 1, end)  
-        cross_max = self.max_crossing_sum(start, mid, end, nums)
+        cross_max = self.max_crossing_sum(start, mid, end, nums)  
 
         return max(left_max, right_max, cross_max)
 
     def max_crossing_sum(self, start, mid, end, nums):
-       
+        
         left_sum = float('-inf')
         total = 0
         for i in range(mid, start - 1, -1):
             total += nums[i]
             left_sum = max(left_sum, total)
 
-    
+        
         right_sum = float('-inf')
         total = 0
         for i in range(mid + 1, end + 1):
