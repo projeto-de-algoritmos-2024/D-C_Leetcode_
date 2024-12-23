8fd17da3efaf5d3256f53c20adb089a3a130bb36
Pedro Augusto Dourado Izarias <pedroaugustoizarias@gmail.com>
2024-12-23 11:09:44 -0300

    Update 53-maximum-subarray.py

--- Questões/Q53-Hard/53-maximum-subarray.py
+++ Questões/Q53-Hard/53-maximum-subarray.py
@@ -1,7 +1,9 @@
 def find_maximum_subarray(nums, start, end):
-    
+    if start == end:  
+        return nums[start]
+   
     pass
 
 def max_crossing_sum(start, mid, end, nums):
-   
+    
     pass
